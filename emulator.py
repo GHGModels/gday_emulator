e71205b6d870ac5fd251cc9c2e58398dd8558e47
mdekauwe <mdekauwe@gmail.com>
2016-06-21 19:23:44 +1000

    cleaned up code

--- emulator.py
+++ emulator.py
@@ -38,79 +38,17 @@ def main(met_fname, gday_outfname, var):
     df = pd.read_csv(gday_outfname, skiprows=3, sep=",", skipinitialspace=True)
     df['date'] = make_data_index(df)
     df = df.set_index('date')
-
     target = df[var].values
 
-    # BUILD MODELS
+    # build emulator
+    regmod = KNeighborsRegressor(n_neighbors=20, weights="distance")
+    regmod.fit(met_data, df[var])
+    predict = regmod.predict(met_data)
 
-    # hold back 40% of the dataset for testing
-    #X_train, X_test, Y_train, Y_test = \
-    #    cross_validation.train_test_split(met_data, target, \
-    #                                      test_size=0.4, random_state=0)
+    df = pd.DataFrame({'DT': df.index, 'emu': predict, 'gday': df[var]})
 
-    """
-    param_DTR = { \
-       #"min_samples_split": [2, 10, 20], \
-       #"max_depth": [2, 5, 10, 30, 50], \
-       #"min_samples_leaf": [1, 5, 10], \
-       #"max_leaf_nodes": [5, 10, 20], \
-       }
-
-    param_RFR = { \
-       #"min_samples_split": [2, 10, 20], \
-       "max_depth": [None], \
-       #"min_samples_leaf": [1, 5, 10], \
-       #"max_leaf_nodes": [5, 10, 20], \
-       #"criterion": ["gini", "entropy"], \
-       "max_features": [2, 4, 8], \
-       #"n_estimators": [5, 10, 20] \
-       "n_estimators": [300], \
-
-    param_SVR = { \
-       "C": [0.5, 1, 2], \
-       "kernel": ['rbf'], \
-       #"coeff": [0, 0.3, 0.5], \
-       "degree": [1, 2, 3], \
-       }
-    """
-
-    param_KNR = { \
-        "n_neighbors": [5, 10, 20], \
-        "weights": ['uniform', 'distance'], \
-        }
-
-    #regmod = DecisionTreeRegressor()
-    #regmod = RandomForestRegressor()
-    #regmod = SVR()
-    regmod = KNeighborsRegressor()
-
-
-    pipeit3 = lambda model: make_pipeline(StandardScaler(), PCA(), model)
-    pipeit2 = lambda model: make_pipeline(StandardScaler(), model)
-    regmod_p = pipeit2(regmod)
-    modlab = regmod_p.steps[-1][0]
-    par_grid = {'{0}__{1}'.format(modlab, parkey): pardat \
-                 for (parkey, pardat) in param_KNR.iteritems()}
-
-    #emulator = GridSearchCV(regmod, param_grid=param_DTR, cv=5)
-    emulator = GridSearchCV(regmod_p, param_grid=par_grid, cv=5)
-
-    #emulator.fit(X_train, Y_train)
-    emulator.fit(met_data, df[var])
-    pred_test = emulator.predict(met_data)
-
-    output_test = pd.DataFrame({'emu': pred_test, \
-                                'gday': df[var]})
-    output_emulated = pd.DataFrame({'DT': df.index, \
-                                    'emu': emulator.predict(met_data), \
-                                    'gday': df[var]})
-    output_emulated.set_index(['DT'], inplace=True)
-    print(output_emulated.head())
-
-    output_day = output_emulated.resample('D').mean()
-
-    plt.plot_date(output_day.index, output_day['emu'], 'o', label='Emulator')
-    plt.plot_date(output_day.index, output_day['gday'], '.', label='GDAY')
+    plt.plot_date(df.index, df['emu'], 'o', label='Emulator')
+    plt.plot_date(df.index, df['gday'], '.', label='GDAY')
     plt.ylabel('GPP (g C m$^{-2}$ s$^{-1}$)')
     plt.legend()
     plt.show()
